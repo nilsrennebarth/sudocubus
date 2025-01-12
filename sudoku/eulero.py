5a239ed4f0f54dbdc55e1fd82aaff5fbeae70b78
Nils Rennebarth <nils.rennebarth@web.de>
2025-01-12 13:09:33 +0100

    Implement pairs in Euleros
    We hold all possible pairs as keys in a dict. The value is
    initialized to the set of all cells. Whenever a cell can be
    excluded from holding a certain pair, the cell is removed from
    the set. When a cell becomes knowns to hold a fixed pair, the set
    is replaced by the cell itself.

--- sudoku/eulero.py
+++ sudoku/eulero.py
@@ -4,8 +4,19 @@ import logging
 
 import cell
 import msquare
+from exceptions import Unsolvable
 
 log = logging.getLogger(__name__)
+
+
+class Eulerosquare(msquare.Magicsquare):
+	"""A magic square inside of an Eulero
+
+	There are only two differences to a regular magic square: An Eulerosquare
+	knows its parent, the Eulero itself, and its position inside it(left: 0 or
+	right: 1), and the setpos method is intercepted to also exclude the
+	corresponding pairs of the eulero.
+	"""
 
 
 class Eulero(types.SimpleNamespace):
@@ -39,8 +50,76 @@ class Eulero(types.SimpleNamespace):
 	def __init__(self, n: int = 5):
 		self.n = n
 		self.square = [msquare.Magicsquare(n) for i in range(2)]
-		everywhere = {c for c in self.square[0].cells}
 		self.pairs = {
-			(i, j): everywhere.copy()
+			(i, j): {c for c in self.square[0].cells}
 			for i in range(n) for j in range(n)
 		}
+
+	def pcell(self, row, col):
+		"""Primary cell at a position"""
+		return self.square[0].cell(row, col)
+
+	def setpair(self, pair:tuple, row, col):
+		"""
+		Set location of a tuple
+		"""
+		pairval = self.pairs[pair]
+		cell = self.pcell(row, col)
+		if isinstance(pairval, cell.BaseCell):
+			# Pair has been found already
+			if pairval.row != row or pairval.col != col:
+				raise Unsolvable(
+					f'Pair {pair} already at ({pairval.row}{pairval.col})'
+				)
+			log.warning(f'Pair {pair} has already been set')
+			return
+		log.debug(f'Pair {pair} found at ({row}, {col})')
+		self.pairs[pair] = self.pcell(row, col)
+		# Remove left value from cells where the right value is already set to
+		# the right value of the pair. Same for the right value
+		for (c0, c1) in zip(self.square[0].cells, self.square[1].cells):
+			if c0 is cell: continue
+			if c0.val == pair[0]:
+				c1.xclude(pair[1])
+			if c1.val == pair[1]:
+				c0.xclude(pair[0])
+
+	def setcell(self, pos:int, row:int, col:int, value:int):
+		"""
+		Remove pairs when a cell receives a value
+
+		Whenever a cell in any of the two magic squares receives a value, we
+		can exclude various location from holding specific pairs.
+
+		If the other cell at the same position already has a value, we must
+		check that this pair hasn't been placed anywhere else, or throw an
+		Unsolvable exception. We then replace the set value of the pair with
+		the (primary) cell at the given position, thus locating the pair.
+
+		Let C be the primary cell at (row, col). For all pairs P that have
+		`value` in position pos, we discard all cells != C from all houses that
+		C is in from the set of P.
+		"""
+		othercell = self.square[1-pos].cell(row, col)
+		# Construct tuples from values at the variable positions pos and 1-pos
+		lpair = [-1, -1]
+		lpair[pos] = value
+		if othercell.is_fix():
+			lpair[1-pos] = othercell.val
+			pair = tuple(lpair)
+			self.setpair(pair, row, col)
+		for otherval in range(self.n):
+			lpair[1-pos] = otherval
+			pair = tuple(lpair)
+			locations = self.pairs[pair]
+			if not isinstance(locations, set):
+				continue
+			current = self.pcell(row, col)
+			for house in self.square[0].houses:
+				for c in house(current):
+					if c is not current:
+						locations.discard(c)
+						if len(locations) == 0:
+							raise Unsolvable(
+								f'No remaining location for pair {pair}'
+							)
